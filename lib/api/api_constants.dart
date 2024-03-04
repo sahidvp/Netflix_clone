fec0c69bca6e8d11ebd305dedb40f63c5f37f206
Sahid vp <sahidsai071@gmail.com>
2024-03-04 11:12:30 +0530

    second commit

--- lib/api/api_constants.dart
+++ lib/api/api_constants.dart
@@ -13,7 +13,7 @@
 // }
 
 class ApiConfig{
-  static const String apiKey = 'b5d6ad1e24212e74aa11ba7db44b830e';
+  static const String apiKey = 'd3a7582a5d124bbca181fb9b8849ce4c';
   static const String baseUrl = 'https://api.themoviedb.org/3';
   static const String imagePath = 'https://image.tmdb.org/t/p/w500';
   static const String popularMovies = '/movie/popular?page=1&api_key=';
